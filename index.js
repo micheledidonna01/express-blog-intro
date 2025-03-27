3ad8db22e3565ca53badbbb4d827b03b15b62df2
micheledidonna01 <micheledidonna337@gmail.com>
2025-03-27 09:59:13 +0100

    add: script menu.js

--- index.js
+++ index.js
@@ -13,38 +13,6 @@ app.get('/', (req, res) => {
 app.get('/bacheca', (req, res) => {
     console.log('Sto chiamando i dolci della pasticceria');
 
-    const menu = [
-        {
-           titolo : "ciambellone",
-           contenuto : "ciambella soffice e genuina",
-           immagine : "img/bacheca/ciambellone.jpeg",
-           tags : ['morbida', 'genuina'] 
-        },
-        {
-           titolo : "cracker alla barbabietola",
-           contenuto : "cracker dolci con impasto di barbabietola",
-           immagine : "img/bacheca/cracker_barbabietola.jpeg",
-           tags : ['croccante'] 
-        },
-        {
-           titolo : "pane fritto",
-           contenuto : "pane fritto dolce",
-           immagine : "img/bacheca/pane_fritto_dolce.jpeg",
-           tags : ['morbido'] 
-        },
-        {
-           titolo : "pasta barbabietola",
-           contenuto : "pasta dolce con impasto di barbabietola",
-           immagine : "img/bacheca/pasta_barbabietola.jpeg",
-           tags : ['unica', 'speziata'] 
-        },
-        {
-           titolo : "torta paesana",
-           contenuto : "ricetta tipica della torta di paese",
-           immagine : "img/bacheca/torta_paesana.jpeg",
-           tags : ['cuore morbido', 'ricoperto al cioccolato'] 
-        },
-    ]
     res.type('JSON').send(menu);
 });
 
@@ -52,3 +20,5 @@ app.listen(port, () => {
     console.log(`Sono un server sulla porta ${port}`);
 });
 
+
+const menu = require("./menu.js");
---
+++ menu.js
@@ -0,0 +1,34 @@
+const menu = [
+    {
+       titolo : "ciambellone",
+       contenuto : "ciambella soffice e genuina",
+       immagine : "img/bacheca/ciambellone.jpeg",
+       tags : ['morbida', 'genuina'] 
+    },
+    {
+       titolo : "cracker alla barbabietola",
+       contenuto : "cracker dolci con impasto di barbabietola",
+       immagine : "img/bacheca/cracker_barbabietola.jpeg",
+       tags : ['croccante'] 
+    },
+    {
+       titolo : "pane fritto",
+       contenuto : "pane fritto dolce",
+       immagine : "img/bacheca/pane_fritto_dolce.jpeg",
+       tags : ['morbido'] 
+    },
+    {
+       titolo : "pasta barbabietola",
+       contenuto : "pasta dolce con impasto di barbabietola",
+       immagine : "img/bacheca/pasta_barbabietola.jpeg",
+       tags : ['unica', 'speziata'] 
+    },
+    {
+       titolo : "torta paesana",
+       contenuto : "ricetta tipica della torta di paese",
+       immagine : "img/bacheca/torta_paesana.jpeg",
+       tags : ['cuore morbido', 'ricoperto al cioccolato'] 
+    },
+];
+
+module.exports = menu;
